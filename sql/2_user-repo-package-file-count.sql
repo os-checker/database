e85e8a2185b003dbad860a0a0f7f48bfafb8972b
zjp <jiping_zhou@foxmail.com>
2024-08-13 22:30:10 +0800

    chore: 定义 report 和 count_on_file 视图

--- sql/2_user-repo-package-file-count.sql
+++ sql/2_user-repo-package-file-count.sql
@@ -1,17 +1,18 @@
 -- 基于 user/repo#package/file 统计问题数量
 -- 如果 count 为 0，则无记录
+CREATE VIEW IF NOT EXISTS report AS
+SELECT
+  json_extract (value, '$.user') AS user,
+  json_extract (value, '$.repo') AS repo,
+  json_extract (value, '$.package') AS package,
+  -- json_extract (value, '$.count') AS count,
+  json_extract (value, '$.raw_reports') AS reports
+FROM
+  t,
+  json_each (raw);
+
+CREATE VIEW IF NOT EXISTS count_on_file AS
 With
-  report AS (
-    SELECT
-      json_extract (value, '$.user') AS user,
-      json_extract (value, '$.repo') AS repo,
-      json_extract (value, '$.package') AS package,
-      -- json_extract (value, '$.count') AS count,
-      json_extract (value, '$.raw_reports') AS reports
-    FROM
-      t,
-      json_each (raw)
-  ),
   files AS (
     SELECT
       *,
@@ -34,3 +35,12 @@ SELECT
   clippy_error
 FROM
   files;
+
+SELECT
+  *
+FROM
+  count_on_file
+ORDER BY
+  user,
+  repo,
+  package;
