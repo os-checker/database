2a37a780d2fa10e98368159f81580bdfd76ef335
zjp <jiping_zhou@foxmail.com>
2024-08-23 16:02:23 +0800

    chore(sh): home/run.sh will produce json to ui/home/

--- home/run.sh
+++ home/run.sh
@@ -1,4 +1,6 @@
 # This script should be run in root dir.
 
-jq -f home/0_all-target-triples.jq tmp/os-checker.json
-jq -f home/1_array-of-target-triples.jq tmp/os-checker.json
+set +e
+
+jq -f home/0_all-target-triples.jq tmp/os-checker.json >ui/home/all-target-triples.json
+jq -f home/1_array-of-target-triples.jq tmp/os-checker.json >ui/home/array-of-target-triples.json
