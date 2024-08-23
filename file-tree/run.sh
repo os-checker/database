e288363d158acc086cb8b45b59cb6929877c7852
zjp <jiping_zhou@foxmail.com>
2024-08-23 19:51:30 +0800

    feat(file-tree): 将 array-of-target-triples.json 切分成小 JSON 文件

--- file-tree/run.sh
+++ file-tree/run.sh
@@ -2,8 +2,15 @@
 
 set +e
 
+mkdir -p ui/file-tree/split/
+
 echo "jq -f file-tree/0_all-target-triples.jq $UI_JSON >ui/file-tree/all-target-triples.json"
 jq -f file-tree/0_all-target-triples.jq "$UI_JSON" >ui/file-tree/all-target-triples.json
 
 echo "jq -f file-tree/1_array-of-target-triples.jq $UI_JSON >ui/file-tree/array-of-target-triples.json"
 jq -f file-tree/1_array-of-target-triples.jq "$UI_JSON" >ui/file-tree/array-of-target-triples.json
+
+# 将 array-of-target-triples.json 按照 target_triple 切分成一个个 JSON 文件
+echo "切分 ui/file-tree/array-of-target-triples.json 文件"
+jq -cr '.[] | {key:.target_triple,data} | "\(.key)\t\(.data)"' ui/file-tree/array-of-target-triples.json |
+  awk -F\\t '{ print $2 >"ui/file-tree/split/" $1 ".json" }'
