2300568416571ebc0dd884f82f9076e0569b6d80
zjp <jiping_zhou@foxmail.com>
2024-08-23 16:11:47 +0800

    chore(sh): echo jq cmds in home/run.sh

--- home/run.sh
+++ home/run.sh
@@ -2,5 +2,8 @@
 
 set +e
 
+echo "jq -f home/0_all-target-triples.jq $UI_JSON >ui/home/all-target-triples.json"
 jq -f home/0_all-target-triples.jq "$UI_JSON" >ui/home/all-target-triples.json
+
+echo "jq -f home/1_array-of-target-triples.jq $UI_JSON >ui/home/array-of-target-triples.json"
 jq -f home/1_array-of-target-triples.jq "$UI_JSON" >ui/home/array-of-target-triples.json
