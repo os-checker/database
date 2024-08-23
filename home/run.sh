699831ee8e0f8e7c92654104505744fbe1d8f9ff
zjp <jiping_zhou@foxmail.com>
2024-08-23 16:05:20 +0800

    chore(sh): respect "$UI_JSON"

--- home/run.sh
+++ home/run.sh
@@ -2,5 +2,5 @@
 
 set +e
 
-jq -f home/0_all-target-triples.jq tmp/os-checker.json >ui/home/all-target-triples.json
-jq -f home/1_array-of-target-triples.jq tmp/os-checker.json >ui/home/array-of-target-triples.json
+jq -f home/0_all-target-triples.jq "$UI_JSON" >ui/home/all-target-triples.json
+jq -f home/1_array-of-target-triples.jq "$UI_JSON" >ui/home/array-of-target-triples.json
