b12d70af18dec6a2c64b4166e986dd70e8fadeb6
zjp <jiping_zhou@foxmail.com>
2024-08-29 09:33:57 +0800

    feat(basic): target-triples.json => basic.json

--- ci.sh
+++ ci.sh
@@ -9,8 +9,8 @@ fi
 
 echo "jq: $(which jq) | version: $(jq --version)"
 
-echo "生成目标架构：ui/target-triple.json"
-jq -fc target-triples.jq $UI_JSON >ui/target-triple.json
+echo "生成目标架构等基本信息：ui/basic.json"
+jq -fc basic.jq $UI_JSON >ui/basic.json
 
 sh ./home/run.sh
 sh ./file-tree/run.sh
