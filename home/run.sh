8105addfd108aae2098eb37d9776864091685627
zjp <jiping_zhou@foxmail.com>
2024-09-05 13:26:00 +0800

    fix(home): 生成各项 JSON 数据时，未能删除旧数据
    ref: https://github.com/os-checker/database/issues/8

--- home/run.sh
+++ home/run.sh
@@ -2,17 +2,23 @@
 
 set +e
 
-mkdir -p ui/home/split/
+export home=ui/home
+export split=$home/split
 
-cmd="jq -f home/0_All-Targets.jq $UI_JSON >ui/home/split/All-Targets.json"
+rm -r "$split" || true
+echo "移除旧的 $split 数据"
+mkdir -p "$split"
+echo "创建 $split"
+
+cmd="jq -f home/0_All-Targets.jq $UI_JSON >$split/All-Targets.json"
 echo "$cmd"
 eval "$cmd"
 
-cmd="jq -f home/1_array-of-target-triples.jq $UI_JSON >ui/home/array-of-target-triples.json"
+cmd="jq -f home/1_array-of-target-triples.jq $UI_JSON >$home/array-of-target-triples.json"
 echo "$cmd"
 eval "$cmd"
 
 # 将 array-of-target-triples.json 按照 target_triple 切分成一个个 JSON 文件
-echo "切分 ui/home/array-of-target-triples.json 文件"
-jq -cr '.[] | {key:.target_triple,data} | "\(.key)\t\(.data)"' ui/home/array-of-target-triples.json |
-  awk -F\\t '{ print $2 >"ui/home/split/" $1 ".json" }'
+echo "切分 $home/array-of-target-triples.json 文件"
+jq -cr '.[] | {key:.target_triple,data} | "\(.key)\t\(.data)"' $home/array-of-target-triples.json |
+  awk -F\\t '{ split_dir = ENVIRON["split"]; print $2 > split_dir "/" $1 ".json" }'
