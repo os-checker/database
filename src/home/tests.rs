a2d75b040d3df7f0dfe0003068e94c1b63bc6208
zjp <jiping_zhou@foxmail.com>
2024-09-07 09:35:50 +0800

    chore(home): 测试反序列化

--- src/home/tests.rs
+++ src/home/tests.rs
@@ -1,5 +1,5 @@
 use super::*;
-use crate::print;
+use crate::{print, Result};
 
 #[test]
 fn all() {
@@ -11,4 +11,13 @@ fn all() {
 fn by_target() {
     let json = &crate::utils::ui_json();
     print(&split_by_target(json));
+}
+
+#[test]
+fn deser() -> Result<()> {
+    let path = "new_ui/batch/home/split/All-Targets/batch_1.json";
+    let home = std::fs::read(path)?;
+    let node: Vec<NodeRepo> = serde_json::from_slice(&home)?;
+    dbg!(node);
+    Ok(())
 }
