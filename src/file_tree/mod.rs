aaf404041f9120cb303472056174b44f15cab77f
zjp <jiping_zhou@foxmail.com>
2024-09-08 10:40:30 +0800

    fix(FileTree): sort by pkg name
    ref: https://github.com/os-checker/database/issues/13

--- src/file_tree/mod.rs
+++ src/file_tree/mod.rs
@@ -55,7 +55,7 @@ fn inner<'a>(json: &'a JsonOutput, data: &[&'a RawData]) -> FileTree<'a> {
     }
 
     // 对 pkg 的计数排序
-    v.sort_unstable_by(|a, b| b.count.cmp(&a.count));
+    v.sort_unstable_by(|a, b| (b.count, b.pkg.pkg).cmp(&(a.count, a.pkg.pkg)));
     // 对文件的计数和文件名排序
     for pkg in &mut v {
         pkg.raw_reports
