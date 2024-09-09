1787d50de57bf38863a5d033d516ff72f9dd562e
zjp <jiping_zhou@foxmail.com>
2024-09-09 10:04:56 +0800

    fix(push.sh): $branch not $(branch)

--- push.sh
+++ push.sh
@@ -6,9 +6,9 @@ if [ -n "$BOT" ]; then
   git add .
   echo "正在提交：$(git status --porcelain)"
   git commit -m "[bot] update batch dir from os-checker CLI repo"
-  echo "提交成功，正在推送到 database 仓库（分支：$(branch)）"
+  echo "提交成功，正在推送到 database 仓库（分支：$branch）"
   git push
-  echo "成功推送到 database 仓库（分支：$(branch)）"
+  echo "成功推送到 database 仓库（分支：$branch）"
 fi
 
 echo 🎇
